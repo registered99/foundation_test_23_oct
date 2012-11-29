ed08ae98c68da9f52ec6579b22a7972aba777917
Nicholas Yoon <s11022ev@gmail.com>
2012-11-29 11:47:04 -0500

    Commiting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025180936) do
+ActiveRecord::Schema.define(:version => 20121113160124) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
