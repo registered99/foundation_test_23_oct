91d911bc14f184b44bd7246efd6deccbaf631fca
Kampla Stoolsin <registered99@gmail.com>
2012-11-29 11:49:31 -0500

    admin page now works
    needed to modify static_pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def contact
   end
+  def admin
+	@products = Product.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
+  end
 end
